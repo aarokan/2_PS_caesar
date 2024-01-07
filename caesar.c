184a525b70d16cb77df3418d4ea266ffa5845908
AR <aarokan@gmail.com>
2024-01-07 23:59:38 +0300

    check cl argument

--- caesar.c
+++ caesar.c
@@ -1,7 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
 
+void checkCommandLineArgument(int k);
+
 int main(int argc, string argv[]) 
 {
 
+}
+
+void checkCommandLineArgument(int k)
+{
+    // code
 }
